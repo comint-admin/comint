5d8912c8789f1ca6100891c2f7d6006c758aa621
David Koplik <13408471+davidkoplik@users.noreply.github.com>
2023-10-15 11:10:31 -0700

    Rewrote index view as class based view

--- comintapp/urls.py
+++ comintapp/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 app_name = 'comintapp'
 urlpatterns = [
-    path('', index_views.handleRequest, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('register/', auth_views.register, name='register'),
     path('login/', auth_views.user_login , name='login'),
     path('logout/', auth_views.user_logout, name='logout'),
--- comintapp/views/index_views.py
+++ comintapp/views/index_views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def handleRequest(request):
-    context = {}
-    if request.user.is_authenticated:
-        # Assuming 'first_name' is a field on your custom user model
-        context['username'] = request.user.first_name if request.user.first_name else request.user.email
-    else:
-        context['username'] = 'User'
-        context['list_of_loans'] = [1,2,3,4]
-    return render(request, "comintapp/index.html", context)
+
+class IndexView(TemplateView):
+    template_name = 'comintapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            # Assuming 'first_name' is a field on your custom user model
+            context['username'] = self.request.user.first_name if self.request.user.first_name else self.request.user.email
+        else:
+            context['username'] = 'User'
+            context['list_of_loans'] = [1, 2, 3, 4]
+        return context
