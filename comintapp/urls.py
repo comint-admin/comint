cd8f0cf2cf19c801c9a30db4e02ec19ef24666c4
therxyy <99295456+ntgantt@users.noreply.github.com>
2023-10-22 16:14:24 -0700

    Improved responsiveness, fixed conflict

--- comintapp/urls.py
+++ comintapp/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = 'comintapp'
 urlpatterns = [
  path('', homepage_views.handleRequest, name='homepage'),
-    path('', index_views.handleRequest, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('register/', auth_views.register, name='register'),
     path('login/', auth_views.user_login , name='login'),
     path('logout/', auth_views.user_logout, name='logout'),
