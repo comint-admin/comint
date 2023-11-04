7485b170dd7559518d99126ed78a0bbd63fde75f
David Koplik <13408471+davidkoplik@users.noreply.github.com>
2023-11-04 13:27:04 -0700

    added hCaptcha to new forms

--- comintapp/forms/auth_forms.py
+++ comintapp/forms/auth_forms.py
@@ -1,15 +1,22 @@
 from django import forms
-from allauth.account.forms import SignupForm
+from allauth.account.forms import SignupForm, LoginForm
 from comintapp.models import ComintUser
 from django.core.exceptions import ValidationError
-from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from hcaptcha_field import hCaptchaField
 
+
+class CoMintSignInForm(LoginForm):
+    captcha = hCaptchaField()
+
+
 class ComintSignupForm(SignupForm):
     first_name = forms.CharField(max_length=30, label='First Name')
     last_name = forms.CharField(max_length=30, label='Last Name', required=False)
+    captcha = hCaptchaField()
+
+    field_order = ['email', 'first_name', 'last_name', 'password1', 'password2', 'captcha']
 
     def clean_email(self):
         email = self.cleaned_data['email']
