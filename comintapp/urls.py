f7f6fd0c33c4b455420a483d5de372d3fd88896c
Shubh Khandelwal <78661257+caffeinelover1012@users.noreply.github.com>
2023-11-03 21:06:45 -0700

    added Bao's signup and login page, added footer

--- comintapp/urls.py
+++ comintapp/urls.py
@@ -5,7 +5,6 @@ from .views import *
 app_name = 'comintapp'
 urlpatterns = [
     path('', index_views.handleRequest, name='index'),
-    path('logout/', LogoutView.as_view(next_page='comintapp:index'), name='logout'),
     path('loans/', loan_views.loanView, name='loans'),
     path('fund_requests/', payment_views.fundRequests, name='fund_requests'),
     path('payments/', payment_views.payments, name='payments')
